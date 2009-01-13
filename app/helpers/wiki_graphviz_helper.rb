8329295287a60b88b3f8644e56cf29acd3ca0be7
Akinori TANAKA <akinori.tanaka@craft-group.jp>
2009-01-13 14:09:18 +0900

    Modify comment

--- app/helpers/wiki_graphviz_helper.rb
+++ app/helpers/wiki_graphviz_helper.rb
@@ -228,7 +228,7 @@ private
 				@view.controller.countup_macro_index()
 				@view.controller.make_macro_output_by_title(macro_params, wiki_id)
 			rescue => e
-				# formatter.rb catch exception and write e.to_s asis. so escape message.
+				# formatter.rb catch exception and write e.to_s as HTML. so escape message.
 				ex = RuntimeError.new(@view.html_escape e.message)
 				ex.set_backtrace(e.backtrace)
 				raise ex
@@ -247,7 +247,7 @@ private
 				@view.controller.countup_macro_index()
 				@view.controller.make_macro_output_by_text(@content.text, macro_params, wiki_id)
 			rescue => e
-				# formatter.rb catch exception and write e.to_s asis. so escape message.
+				# formatter.rb catch exception and write e.to_s as HTML. so escape message.
 				ex = RuntimeError.new(@view.html_escape e.message)
 				ex.set_backtrace(e.backtrace)
 				raise ex
