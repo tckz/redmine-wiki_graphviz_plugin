6462c58f7d96570451b535fe42bd98ed1c6621c3
tckz <at.tckz@gmail.com>
2010-01-11 23:56:12 +0900

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
 	if (Redmine::VERSION.to_a <=> [0, 9, 0]) >= 0
-		# 0.9.0 or higer
+		# 0.9.0 or higher
 		map.connect 'projects/:id/wiki/:page/graphviz', :controller => 'wiki_graphviz', :action => 'graphviz'
 	else
 		map.connect 'wiki/:id/:page/graphviz', :controller => 'wiki_graphviz', :action => 'graphviz'
