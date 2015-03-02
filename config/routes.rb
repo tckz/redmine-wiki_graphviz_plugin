d0aaf81b3fac70a857c57f4f6ee5c8c09e7d2f35
Mate GABRI <mate@gabri.hu>
2015-03-03 00:05:41 +0100

    use get in routes instead of match

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RedmineApp::Application.routes.draw do
-	match 'projects/:project_id/wiki/:id/graphviz', :to => 'wiki_graphviz#graphviz'
+	get 'projects/:project_id/wiki/:id/graphviz', :to => 'wiki_graphviz#graphviz'
 end
 
 # vim: set ts=2 sw=2 sts=2:
